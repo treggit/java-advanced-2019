0f73bf22fb2fe8ec2952c322aa12ed01a53c2c84
Georgiy Korneev <kgeorgiy@keorgiy.info>
2019-03-18 09:09:04 +0300

    Improved tests for Concurrent homework published

--- modules/info.kgeorgiy.java.advanced.concurrent/info/kgeorgiy/java/advanced/concurrent/ScalarIPTest.java
+++ modules/info.kgeorgiy.java.advanced.concurrent/info/kgeorgiy/java/advanced/concurrent/ScalarIPTest.java
@@ -1,13 +1,17 @@
 package info.kgeorgiy.java.advanced.concurrent;
 
+import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
 
 /**
- * Basic tests for easy version
+ * Full tests for easy version
  * of <a href="https://www.kgeorgiy.info/courses/java-advanced/homeworks.html#homework-implementor">Implementor</a> homework
  * for <a href="https://www.kgeorgiy.info/courses/java-advanced/">Java Advanced</a> course.
  *
@@ -15,6 +19,17 @@ import java.util.Collections;
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ScalarIPTest<P extends ScalarIP> extends BaseIPTest<P> {
+    public static final int PROCESSORS = Runtime.getRuntime().availableProcessors();
+
+    public static final Comparator<Integer> SLEEP_COMPARATOR = (o1, o2) -> {
+        try {
+            Thread.sleep(10);
+        } catch (final InterruptedException e) {
+            e.printStackTrace();
+        }
+        return Integer.compare(o1, o2);
+    };
+
     @Test
     public void test01_maximum() throws InterruptedException {
         test(Collections::max, ScalarIP::maximum, comparators);
@@ -34,4 +49,56 @@ public class ScalarIPTest<P extends ScalarIP> extends BaseIPTest<P> {
     public void test04_any() throws InterruptedException {
         test((data, predicate) -> data.stream().anyMatch(predicate), ScalarIP::any, predicates);
     }
+
+    @Test
+    public void test05_sleepPerformance() throws InterruptedException {
+        final List<Integer> data = randomList(100 * PROCESSORS);
+        final double speedup = speedup(data, SLEEP_COMPARATOR, PROCESSORS * 2);
+        Assert.assertTrue("Not parallel", speedup > 0.66);
+    }
+
+    protected double speedup(final List<Integer> data, final Comparator<Integer> comparator, final int threads) throws InterruptedException {
+        System.err.println("    Warm up");
+        final ConcurrentFunction<P, Integer, Comparator<Integer>> maximum = ScalarIP::maximum;
+        for (int i = 0; i < 5; i++) {
+            performance(threads, threads, data, maximum, comparator);
+        }
+        System.err.println("    Measurement");
+
+        final double performance1 = performance(1, threads, data, maximum, comparator);
+        final double performance2 = performance(threads, threads, data, maximum, comparator);
+        final double speedup = performance2 / performance1;
+        System.err.format("    Performance ratio %.1f for %d threads (%.1f %.1f ms/op)%n", speedup, threads, performance1, performance2);
+        return speedup;
+    }
+
+    protected int getSubtasks(final int threads, final int totalThreads) {
+        return threads;
+    }
+
+    private double performance(final int threads, final int totalThreads, final List<Integer> data, final ConcurrentFunction<P, Integer, Comparator<Integer>> f, final Comparator<Integer> comparator) throws InterruptedException {
+        final int subtasks = getSubtasks(threads, totalThreads);
+        final long start = System.nanoTime();
+        f.apply(createInstance(threads), subtasks, data, comparator);
+        final long time = System.nanoTime() - start;
+
+        final double ops = (subtasks - 1) + (Math.ceil(data.size() / (double) threads) - 1);
+        return time / 1e6 / ops;
+    }
+
+    protected final List<Named<Comparator<Integer>>> comparators = List.of(
+            new Named<>("Natural order", Integer::compare),
+            new Named<>("Reverse order", (l1, l2) -> Integer.compare(l2, l1)),
+            new Named<>("Div 100", Comparator.comparingInt(v -> v / 100)),
+            new Named<>("Even first", Comparator.<Integer>comparingInt(v -> v % 2).thenComparing(v -> v)),
+            new Named<>("All equal", (v1, v2) -> 0)
+    );
+
+    protected final List<Named<Predicate<Integer>>> predicates = List.of(
+            new Named<>("Equal 0", Predicate.isEqual(0)),
+            new Named<>("Greater than 0", i -> i > 0),
+            new Named<>("Even", i -> i % 2 == 0),
+            new Named<>("True", i -> true),
+            new Named<>("False", i -> false)
+    );
 }
