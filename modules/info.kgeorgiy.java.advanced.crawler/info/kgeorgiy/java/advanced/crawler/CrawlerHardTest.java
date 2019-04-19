5e37eeb8d6c10589c06e8ce1c81694949bf61ae3
Georgiy Korneev <kgeorgiy@keorgiy.info>
2019-04-19 11:20:52 +0300

    Modification for Crawler homework added

--- modules/info.kgeorgiy.java.advanced.crawler/info/kgeorgiy/java/advanced/crawler/CrawlerHardTest.java
+++ modules/info.kgeorgiy.java.advanced.crawler/info/kgeorgiy/java/advanced/crawler/CrawlerHardTest.java
@@ -16,4 +16,17 @@ public class CrawlerHardTest extends CrawlerEasyTest {
     public void test10_singleConnectionPerHost() throws IOException {
         test("http://www.ifmo.ru", 2, Integer.MAX_VALUE, Integer.MAX_VALUE, 1, 10, 10);
     }
+
+    @Test
+    public void test11_limitedConnectionsPerHost() throws IOException {
+        test("http://www.ifmo.ru", 2, Integer.MAX_VALUE, Integer.MAX_VALUE, 10, 10, 10);
+    }
+
+    @Test
+    public void test12_limitedConnectionsPerformance() throws IOException {
+        final long time = test("http://www.ifmo.ru", 2, Integer.MAX_VALUE, Integer.MAX_VALUE, 3, 100, 100);
+        System.out.println("Time: " + time);
+        Assert.assertTrue("Too parallel", time > 2000);
+        Assert.assertTrue("Not parallel", time < 4000);
+    }
 }
